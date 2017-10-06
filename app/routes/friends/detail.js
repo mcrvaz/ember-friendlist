2930e3902909ff375eee3c29108ca8ab5647b6e0
Marcello Vaz <marcello.vaz@finxi.com.br>
2017-10-06 20:44:14 -0300

    Friend detail finished

--- app/routes/friends/detail.js
+++ app/routes/friends/detail.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function(id) {
+    model(params) {
+        let id = params.friend_id;
         return this.get('store').findRecord('friend', id);
     }
 });
--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        let id = params.friend_id;
+        return this.get('store').findRecord('friend', id);
+    }
 });
