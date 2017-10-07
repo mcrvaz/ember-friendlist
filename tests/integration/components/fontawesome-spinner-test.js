e6bfef14ac006e02ba37ce1dcc07e70803cedfb1
Marcello Vaz <marcello.vaz@finxi.com.br>
2017-10-07 20:44:11 -0300

    Adjusting whitespaces

--- tests/integration/components/fontawesome-spinner-test.js
+++ tests/integration/components/fontawesome-spinner-test.js
@@ -2,23 +2,23 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('fontawesome-spinner', 'Integration | Component | fontawesome spinner', {
-  integration: true
+    integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{fontawesome-spinner}}`);
+    this.render(hbs`{{fontawesome-spinner}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#fontawesome-spinner}}
-      template block text
-    {{/fontawesome-spinner}}
-  `);
+    // Template block usage:
+    this.render(hbs`
+        {{#fontawesome-spinner}}
+            template block text
+        {{/fontawesome-spinner}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(this.$().text().trim(), 'template block text');
 });
